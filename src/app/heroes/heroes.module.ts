70c34c42cae8566809827f8509ede62f0bc1beaf
Oskar Bazadua <oskkarbazaldua@gmail.com>
2023-05-22 22:04:54 -0600

    Update 22

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -13,6 +13,7 @@ import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { CardComponent } from './components/card/card.component';
 import { HeroImagePipe } from './pipes/hero-image.pipe';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { HeroImagePipe } from './pipes/hero-image.pipe';
     NewPageComponent,
     SearchPageComponent,
     CardComponent,
-    HeroImagePipe
+    HeroImagePipe,
+    ConfirmDialogComponent
   ],
   imports: [
     CommonModule,
