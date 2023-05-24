17a72bd3fb2e8b6389c10b8285e7a84cb5d0b6a2
Oskar Bazadua <oskkarbazaldua@gmail.com>
2023-05-23 21:17:04 -0600

    23 Update

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, map, of, tap } from 'rxjs';
+
+import { environments } from 'src/environments/environments';
+import { User } from '../interfaces/user.inteface';
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+
+
+  private baseUrl = environments.baseUrl;
+  private user?: User;
+
+  constructor(private http: HttpClient) { }
+
+  get currentUser():User|undefined {
+    if ( !this.user ) return undefined;
+    return structuredClone( this.user );
+  }
+
+  login( email: string, password: string ):Observable<User> {
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap ( user => this.user = user),
+        tap ( user => localStorage.setItem( 'token', 'he3rQWDDNnjkh.dsqH' )),
+      );
+  }
+
+
+  checkAuthentication(): Observable<boolean>{
+    if ( !localStorage.getItem('token') ) return of (false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user=> this.user = user ),
+        map( user => !!user ),
+        catchError( err => of(false))
+      );
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
+  }
+
+}
